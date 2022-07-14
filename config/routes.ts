761778457298ad587a9eabe40d8463104a560ed5
sysatom <sysatom@gmail.com>
2022-07-14 15:24:59 +0800

    UI(menu): update routes

--- config/routes.ts
+++ config/routes.ts
@@ -20,16 +20,52 @@
     component: './Welcome',
   },
   {
-    path: '/admin',
-    name: 'admin',
+    name: 'workflows',
+    icon: 'table',
+    path: '/workflows',
+    component: './TableList',
+  },
+  {
+    path: '/security',
+    name: 'security',
     icon: 'crown',
     access: 'canAdmin',
     routes: [
       {
-        path: '/admin/sub-page',
-        name: 'sub-page',
+        path: '/security/users',
+        name: 'users',
         icon: 'smile',
-        component: './Welcome',
+        component: './TableList',
+      },
+      {
+        path: '/security/roles',
+        name: 'roles',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/security/user-statistics',
+        name: 'user-statistics',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/security/actions',
+        name: 'actions',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/security/resources',
+        name: 'resources',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/security/permissions',
+        name: 'permissions',
+        icon: 'smile',
+        component: './TableList',
       },
       {
         component: './404',
@@ -37,10 +73,62 @@
     ],
   },
   {
-    name: 'list.table-list',
-    icon: 'table',
-    path: '/list',
-    component: './TableList',
+    path: '/logs',
+    name: 'logs',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/logs/runs',
+        name: 'runs',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/logs/jobs',
+        name: 'jobs',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/logs/audit',
+        name: 'audit',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/logs/workers',
+        name: 'workers',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      {
+        path: '/admin/variables',
+        name: 'variables',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        path: '/admin/connections',
+        name: 'connections',
+        icon: 'smile',
+        component: './TableList',
+      },
+      {
+        component: './404',
+      },
+    ],
   },
   {
     path: '/',
