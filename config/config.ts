6d66c874a9410b3e8a7d9c32983636b83cfb45d9
sysatom <sysatom@gmail.com>
2023-05-10 22:56:54 +0800

    fix targets

--- config/config.ts
+++ config/config.ts
@@ -27,9 +27,14 @@ export default defineConfig({
     baseNavigator: true,
   },
 
-  targets: {
-    ie: 11,
-  },
+  /**
+   * @name 兼容性设置
+   * @description 设置 ie11 不一定完美兼容，需要检查自己使用的所有依赖
+   * @doc https://umijs.org/docs/api/config#targets
+   */
+  // targets: {
+  //   ie: 11,
+  // },
   // umi routes: https://umijs.org/docs/routing
   routes,
   access: {},
